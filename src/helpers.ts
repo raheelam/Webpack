329821a6c34a6019d0b5f59bfc2a6a2f471b1fca
raheelam <raheelamoha@gmail.com>
2024-01-04 22:48:57 +0300

    adds capitalizeFirstLetter function

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,4 +4,8 @@ export class StringHelper{
 static isEmpty=(str:string)=>{
     return (str && str.trim().length>0);
 }
+
+static capitalizeFirstLetter=(str: string) =>{
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 }
