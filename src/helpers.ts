bdf6e42217ae6d60b0c2f23f21aa39d503bfd70e
raheelam <raheelamoha@gmail.com>
2024-01-15 23:04:19 +0300

    adds function for generating unique id

--- src/helpers.ts
+++ src/helpers.ts
@@ -155,3 +155,10 @@ export const isAlphanumeric=(str: string): boolean =>{
   return /^[a-zA-Z0-9]+$/.test(str);
 }
 
+const generateUniqueId=(): string =>{
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
