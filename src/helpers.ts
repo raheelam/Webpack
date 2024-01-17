c658a4af8baf4d31e4de9e53e8bdd60e7e2fdf49
raheelam <raheelamoha@gmail.com>
2024-01-17 19:29:21 +0300

    adds isToday function

--- src/helpers.ts
+++ src/helpers.ts
@@ -197,3 +197,8 @@ export const  base64ToBlob=(base64String: string, mimeType: string): Blob=> {
 
   return new Blob(byteArrays, { type: mimeType });
 }
+
+export const  isToday=(date: Date): boolean=> {
+  const today = new Date();
+  return date.getDate() === today.getDate() && date.getMonth() === today.getMonth() && date.getFullYear() === today.getFullYear();
+}
