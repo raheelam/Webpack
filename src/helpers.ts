d8b39a47f85ee00e5719952deded414a42021f9d
raheelam <raheelamoha@gmail.com>
2024-01-18 23:29:48 +0300

    adds function for checking valid json

--- src/helpers.ts
+++ src/helpers.ts
@@ -240,3 +240,12 @@ export const  deepEqual=(obj1: any, obj2: any): boolean=> {
 const removeDuplicatesFromArray=<T>(array: T[]): T[] =>{
   return Array.from(new Set(array));
 }
+
+const isValidJSON=(str: string): boolean =>{
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
