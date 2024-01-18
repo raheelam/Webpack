32d6feceae9dcef29f4c6c8aeda147af66851145
raheelam <raheelamoha@gmail.com>
2024-01-18 23:31:39 +0300

    adds function for generating date range

--- src/helpers.ts
+++ src/helpers.ts
@@ -249,3 +249,15 @@ const isValidJSON=(str: string): boolean =>{
     return false;
   }
 }
+
+const generateDateRange=(startDate: Date, endDate: Date): Date[]=> {
+  const dates: Date[] = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= endDate) {
+    dates.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
+}
