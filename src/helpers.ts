dae1e98a6092933f79f97f7b49f168dca421f4e8
raheelam <raheelamoha@gmail.com>
2024-01-21 23:10:08 +0300

    adds a function that checks if element is in viewport

--- src/helpers.ts
+++ src/helpers.ts
@@ -342,3 +342,13 @@ export const makeGetRequest=(url, headers = {})=> {
 export const uniqueArrayValues=(array)=> {
   return Array.from(new Set(array));
 }
+
+export const isInViewport=(element)=> {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
