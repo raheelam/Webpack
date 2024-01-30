5ef65ec351685465adfdbd42dc0d2d2f0d95d20f
raheelam <raheelamoha@gmail.com>
2024-01-30 20:34:43 +0300

    adds algorithm for returning indices of numbers that sum up to a target in all cases

---
+++ src/algorithms/arrays.js
@@ -0,0 +1,24 @@
+//Given an array of integers num and an integer target,
+//return indices of the two numbers such that they add up to target
+
+const findValuesThatMakeUpTarget = (numArray, target) => {
+  let missingNumber = 0;
+  const valuesArray = [];
+  const arrayAsObject = numArray.reduce((accumulator, value, index) => ({
+    ...accumulator,
+    [value]: [value, index],
+  }));
+
+  for (let index in numArray) {
+    const value = numArray[index];
+    missingNumber = target - value;
+
+    if (arrayAsObject[missingNumber]) {
+      valuesArray.push([arrayAsObject[missingNumber][1], Number(index)]);
+    }
+  }
+
+  return valuesArray;
+};
+
+console.log(findValuesThatMakeUpTarget([1, 2, 3, 4, 5, 6, 7], 11));
